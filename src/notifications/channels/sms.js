51288d14c5d3a548befacdc8654903cd0f03a838
Mohammad Abu Khadra <mohammad.abukhadra@optimumpartners.co>
2022-03-27 15:19:41 +0300

    fix twilio error

--- src/notifications/channels/sms.js
+++ src/notifications/channels/sms.js
@@ -1,7 +1,11 @@
 const { twilioAccountSid, twilioAuthToken, smsSender } = require('config/config');
 const Twilio = require('twilio');
 
-const client = new Twilio(twilioAccountSid, twilioAuthToken);
+let client;
+
+if (twilioAccountSid && twilioAuthToken) {
+  client = new Twilio(twilioAccountSid, twilioAuthToken);
+}
 
 /**
  * Send SMS message.
@@ -11,6 +15,11 @@ const client = new Twilio(twilioAccountSid, twilioAuthToken);
  *
  * @return {Promise<import('twilio/lib/rest/api/V2010/account/message').MessageInstance>}
  */
-const send = async (to, body) => client.messages.create({ body, to, from: smsSender });
+const send = async (to, body) => {
+  if (!client) {
+    throw new Error('Twilio environment variables are not set');
+  }
+  return client.messages.create({ body, to, from: smsSender });
+};
 
 module.exports = send;
